1b65667127587b9c2623073e74053166179c4fbc
Aleksey Kharlanov <harlan14@mail.ru>
2025-11-08 14:29:56 +0700

    change 4 file +

--- src/test/java/BlankPage.java
+++ src/test/java/BlankPage.java
@@ -29,7 +29,7 @@ public class BlankPage {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
